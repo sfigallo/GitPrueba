68c63b6bcad46b93db7848c8b71922d1c80d12f9
ramirojimenez094 <ramirojimenez094@gmail.com>
2015-09-22 10:56:45 -0300

    listo para el pull request

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Est0 es una prueba de GitHub");
-		System.out.println("Otro tipo de prueba");
+		System.out.println("Otr000 tipo de prueba");
 
 	}
 
