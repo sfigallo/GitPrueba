d02f5563d316db041c72213e3588ed886c60ea99
ramirojimenez094 <ramirojimenez094@gmail.com>
2015-09-22 10:46:55 -0300

    prueba de merge

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -2,8 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es una prueba de GitHub");
-		System.out.println("Otr0 tipo de prueba");
+		System.out.println("Est0 es una prueba de GitHub");
+		System.out.println("Otr000 tipo de prueba");
 
 	}
 
